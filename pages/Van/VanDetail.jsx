b87054125185eb0abca928c54dd26aef85fee859
Jeetender1621 <160727550+Jeetender1621@users.noreply.github.com>
2025-05-06 23:28:28 +0530

    adding defer and await for vanDetails page

--- pages/Van/VanDetail.jsx
+++ pages/Van/VanDetail.jsx
@@ -1,15 +1,35 @@
-import { useEffect, useState } from "react";
-import { useParams, Link, useLocation, useLoaderData } from "react-router-dom";
+import { Suspense, useEffect, useState } from "react";
+import {
+  useParams,
+  Link,
+  useLocation,
+  useLoaderData,
+  defer,
+  Await,
+} from "react-router-dom";
 import { getVans } from "../../utilities/vanAPIs";
 
 export function getVanDetail({ params }) {
-  return getVans(params.id);
+  return defer({ vanInfo: getVans(params.id) });
 }
 
 export default function VanDetail() {
   // const { id } = useParams();
   const location = useLocation();
-  const vanDetail = useLoaderData();
+  const vanDetailPromise = useLoaderData();
+
+  const renderVanDetails = (vanDetail) => (
+    <div className="van-detail">
+      <img src={vanDetail.imageUrl} />
+      <i className={`van-type ${vanDetail.type} selected`}>{vanDetail.type}</i>
+      <h2>{vanDetail.name}</h2>
+      <p className="van-price">
+        <span>${vanDetail.price}</span>/day
+      </p>
+      <p>{vanDetail.description}</p>
+      <button className="link-button">Rent this van</button>
+    </div>
+  );
 
   return (
     <div className="van-detail-container">
@@ -28,18 +48,11 @@ export default function VanDetail() {
         </span>
       </Link>
 
-      <div className="van-detail">
-        <img src={vanDetail.imageUrl} />
-        <i className={`van-type ${vanDetail.type} selected`}>
-          {vanDetail.type}
-        </i>
-        <h2>{vanDetail.name}</h2>
-        <p className="van-price">
-          <span>${vanDetail.price}</span>/day
-        </p>
-        <p>{vanDetail.description}</p>
-        <button className="link-button">Rent this van</button>
-      </div>
+      <Suspense fallback={<h2>Loading van details...</h2>}>
+        <Await resolve={vanDetailPromise.vanInfo}>
+          {(vanDetail) => renderVanDetails(vanDetail)}
+        </Await>
+      </Suspense>
     </div>
   );
 }
